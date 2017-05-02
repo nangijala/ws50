53f2c988dfd4511d2b8659275bb5c1299914640e
Markus Knecht <markus.knecht@psi.ch>
2017-05-02 20:28:13 +0200

    Update: Version mit alten Sound-Dateien (Nummern)

--- ws50.ino
+++ ws50.ino
@@ -1,8 +1,5 @@
 #include <Event.h>
 #include <Timer.h>
-
-
-
 #include <SPI.h>
 #include <SdFat.h>
 #include <SdFatUtil.h>
@@ -35,6 +32,92 @@ class Msg{
 
 StackArray <String> stack;
 
+void overTime();
+
+#define NUMLEN  3
+#define MAX_BUFFER  10
+
+class NumberTracker{
+
+  int waitFor[NUMLEN+1] = {1,1,8};
+  int numbers[MAX_BUFFER+1];
+  int index = 0;
+  int timerId = -1;
+  
+  public:
+  
+  NumberTracker(){
+   reset();
+  }
+
+  stopTimer(){
+    if( timerId > -1)
+      timer.stop( timerId );
+    timerId = -1;
+    
+  }
+  
+  restartTimer(){
+    stopTimer();
+    timerId = timer.after(5000, overTime);
+  }
+  
+  add( int number){
+    if( index < MAX_BUFFER ){
+      restartTimer();
+      numbers[ index++ ] = number;
+      return true;      
+    }
+    return false;
+  }
+  
+  reset(){
+    stopTimer();    
+    index = 0;
+    for(int i=0; i< MAX_BUFFER; i++){
+      numbers[i] = -1;
+    }
+  }
+
+  checkIsCorrectNumber(){
+    for(int i=0; i< NUMLEN; i++){      
+      if( i > index){
+        return false;
+      }
+        
+      if( numbers[i] != waitFor[i])
+        return false;
+    }
+    reset();
+    return true;
+  }
+
+  checkIsWrongNumber(){
+    if( !isDialing())
+      return false;
+      
+    for(int i=0; i< index; i++){
+      /*
+      Serial.print("Vergleiche ");
+      Serial.print( i );      
+      Serial.print( numbers[i] );
+      Serial.print( waitFor[i] );
+      */
+      if( i > NUMLEN || numbers[i] != waitFor[i]){
+        reset();
+        return true;
+      }
+    }
+    return false;   
+  }
+
+  isDialing(){
+    return index>0 ? true : false;
+  }
+    
+};
+
+NumberTracker tracker = NumberTracker();
 
 RotaryDialer dialer = RotaryDialer(PIN_READY, PIN_PULSE);
 
@@ -66,26 +149,52 @@ void loop() {
   timer.update();
   
   b_Gabel.update();
+
+  // Get the updated value :
+  int gabeValue = b_Gabel.read();
+    
   if( b_Gabel.fell()){
      stack.push( String("welcome.mp3") );
   }
-  
-  if (dialer.update()) {        
-    String msg = "zahl" + String(dialer.getNextNumber()) + ".mp3";
-    stack.push( msg );
+  if( b_Gabel.rose()){
+     Serial.println("Aufgelegt");
+    tracker.reset();     
   }
 
+  
+  if (dialer.update()) {  
+    int theNumber = dialer.getNextNumber();       
+    String msg = "zahl" + String(theNumber) + ".mp3";
+    stack.push( msg );
+    tracker.add(theNumber);
+  }
+
+  if( tracker.checkIsCorrectNumber() == true){
+    Serial.println( "Korrekt");
+  }
+
+  if( tracker.checkIsWrongNumber() ){
+    Serial.println( "Falsch Verbunden");
+  }
+ 
   if( stack.count() > 0 && bTimerRunning == false ){
     String a = stack.peek();
     timer.after( 300, nextSong);
     bTimerRunning = true;
    }
+   
 }
 
-
+void overTime(){   
+   Serial.println( "Abgelaufen");
+   tracker.reset();
+}
+  
 void nextSong(){
    if( stack.count() > 0){
-    MP3player.playMP3(stack.pop().c_str());
+    String song = stack.pop();
+    MP3player.playMP3(song.c_str());
    }
    bTimerRunning = false;
  }
+ 
